3b0428ba7674855fa3f281f1cda4f25822f0ce73
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2022-08-25 17:20:59 -0500

    Test git changes

--- catkin_ws/src/students/scripts/practice01.py
+++ catkin_ws/src/students/scripts/practice01.py
@@ -13,7 +13,7 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "FULL NAME"
+NAME = "MARCO NEGRETE"
 
 def callback_scan(msg):
     global obstacle_detected
