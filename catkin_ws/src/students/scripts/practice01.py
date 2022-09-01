75f8211bde481968e96f99effd7fd7a9f9f1b0e2
Junior3111 <33918729+Junior3111@users.noreply.github.com>
2022-09-01 13:31:54 -0500

    Update practice01.py
    nombre completo

--- catkin_ws/src/students/scripts/practice01.py
+++ catkin_ws/src/students/scripts/practice01.py
@@ -13,7 +13,7 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "DANIEL NAJERA"
+NAME = "Daniel Najera Araoz"
 
 def callback_scan(msg):
     global obstacle_detected
