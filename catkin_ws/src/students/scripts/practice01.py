69e595d2b1af6e7f6b82d8ca4ddd7e112c096653
DanielChavolla <111928606+DanielChavolla@users.noreply.github.com>
2022-08-31 23:37:07 -0500

    Update practice01.py

--- catkin_ws/src/students/scripts/practice01.py
+++ catkin_ws/src/students/scripts/practice01.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python3
-#
-# MOBILE ROBOTS - FI-UNAM, 2022-2
 # PRACTICE 01 - THE PLATFORM ROS 
 #
 # Instructions:
 # Write a program to move the robot forwards until the laser
 # detects an obstacle in front of it.
 # Required publishers and subscribers are already declared and initialized.
-#
+#probando1
 
 import rospy
 from sensor_msgs.msg   import LaserScan
@@ -22,7 +19,8 @@ def callback_scan(msg):
     # Do something to detect if there is an obstacle in front of the robot.
     # Set the 'obstacle_detected' variable with True or False, accordingly.
     #
-    
+    n = int((msg.angle_max - msg.angle_min)/msg.angle_increment/2)
+    obstacle_detected = msg.ranges[n] < 1.0
     return
 
 def main():
@@ -41,7 +39,9 @@ def main():
         # Move forward if there is no obstacle in front of the robot, and stop otherwise.
         # Use the 'obstacle_detected' variable to check if there is an obstacle. 
         # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
-        #
+        msg_cmd:vel = Twist()
+        msg_cmd_vel.linear.x = 0 if obstacle_detected else 0.3
+        pub_cmd_vel.publish(msg_cmd_vel)
         
         loop.sleep()
 
