16edba3cf1bddeea99df57d7da8bd81309aceff7
LFernandoGO <luisfergtz99@hotmail.com>
2022-09-07 20:00:57 -0500

    push

--- catkin_ws/src/students/scripts/practice01.py
+++ catkin_ws/src/students/scripts/practice01.py
@@ -13,7 +13,7 @@ import rospy
 from sensor_msgs.msg   import LaserScan
 from geometry_msgs.msg import Twist
 
-NAME = "Gutierrez Orozpe Luis Fernando "
+NAME = "Gutierrez Orozpe Luis Fernando"
 
 def callback_scan(msg):
     global obstacle_detected
