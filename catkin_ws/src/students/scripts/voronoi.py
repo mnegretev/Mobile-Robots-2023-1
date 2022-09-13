2e1540c0fcd9209336f62d170c52e8ae43705a44
Junior3111 <33918729+Junior3111@users.noreply.github.com>
2022-09-13 14:41:23 -0500

    Update voronoi.py

--- catkin_ws/src/students/scripts/voronoi.py
+++ catkin_ws/src/students/scripts/voronoi.py
@@ -20,7 +20,7 @@ from nav_msgs.srv import GetMap
 from nav_msgs.srv import GetMapResponse
 from nav_msgs.srv import GetMapRequest
 
-NAME = "FULL_NAME"
+NAME = "Daniel Najera Araoz"
 
 def brushfire(grid_map):
     print("Executing brushfire algorithm...")
