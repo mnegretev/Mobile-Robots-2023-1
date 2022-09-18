6146fe907337dda1a229364cb4b183a59913181b
JoseSoto9919 <josemsmm9919@gmail.com>
2022-09-18 12:58:27 -0500

    practica 3

--- catkin_ws/src/students/scripts/practice03b.py
+++ catkin_ws/src/students/scripts/practice03b.py
@@ -18,7 +18,7 @@ from nav_msgs.msg import Path
 from nav_msgs.srv import *
 from collections import deque
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "SOTO MONTERROZA"
 
 msg_path = Path()
 
