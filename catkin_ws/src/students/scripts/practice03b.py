93b7a129635a9066ac01f8994cd7230eba3f7253
JavierTroncosom <30993711+JavierTroncosom@users.noreply.github.com>
2022-09-18 10:55:42 -0500

    Update practice03b.py

--- catkin_ws/src/students/scripts/practice03b.py
+++ catkin_ws/src/students/scripts/practice03b.py
@@ -18,7 +18,7 @@ from nav_msgs.msg import Path
 from nav_msgs.srv import *
 from collections import deque
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "Troncoso Moreno Javier Adan"
 
 msg_path = Path()
 
