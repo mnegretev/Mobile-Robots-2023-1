400ecf2f350cd6c3513cd66baf7f31170e139ba1
JavierTroncosom <adan.troncoso@live.com.mx>
2022-09-25 15:24:34 -0500

    cambios practica3

--- catkin_ws/src/students/scripts/practice03b.py
+++ catkin_ws/src/students/scripts/practice03b.py
@@ -18,7 +18,7 @@ from nav_msgs.msg import Path
 from nav_msgs.srv import *
 from collections import deque
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "Troncoso Moreno Javier Adan"
 
 msg_path = Path()
 
@@ -65,8 +65,9 @@ def a_star(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
             #
             g = g_values[row, col] + abs(row-r) + abs(col-c) + cost_map[r][c]
             h = abs(goal_r - r) + abs(goal_c - c)
-            # g = g_values[row, col] + math.sqrt((row-r)**2 + (col - c)**2) + cost_map[r][c]
-            # h = math.sqrt((goal_r-r)**2 + (goal_c - c)**2)
+            #g = g_values[row, col] + math.sqrt((row-r)**2 + (col - c)**2) + cost_map[r][c]
+            #h = math.sqrt((goal_r-r)**2 + (goal_c - c)**2)
+            #h=0
             #
             
             f = g + h                         
