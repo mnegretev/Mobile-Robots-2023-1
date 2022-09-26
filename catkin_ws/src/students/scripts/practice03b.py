0d1b494997404602ed886cfccee7e365e106a4a3
jorale2133 <jorale2133@gmail.com>
2022-09-26 00:33:47 -0300

    Modificasiòn de la Pràctica04

--- catkin_ws/src/students/scripts/practice03b.py
+++ catkin_ws/src/students/scripts/practice03b.py
@@ -63,10 +63,10 @@ def a_star(start_r, start_c, goal_r, goal_c, grid_map, cost_map):
             # Modify calculations of 'g' and 'h' to use euclidean distance
             # instead of Manhattan distance
             #
-            # g = g_values[row, col] + abs(row-r) + abs(col-c) + cost_map[r][c]
-            # h = abs(goal_r - r) + abs(goal_c - c)
-            g = g_values[row, col] + math.sqrt((row-r)**2 + (col - c)**2) + cost_map[r][c]
-            h = 0
+            g = g_values[row, col] + abs(row-r) + abs(col-c) + cost_map[r][c]
+            h = abs(goal_r - r) + abs(goal_c - c)
+            #g = g_values[row, col] + math.sqrt((row-r)**2 + (col - c)**2) + cost_map[r][c]
+            #h = 0
             
             
             f = g + h                         
