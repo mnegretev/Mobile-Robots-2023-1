ca4eee61f67872f41109ca4d01ee8e4bb5a04979
danieltorres4 <dst990502@outlook.com>
2022-10-12 12:24:46 -0500

    Adding the control law

--- catkin_ws/src/students/scripts/practice05.py
+++ catkin_ws/src/students/scripts/practice05.py
@@ -18,7 +18,7 @@ from nav_msgs.srv import GetPlan, GetPlanRequest
 from custom_msgs.srv import SmoothPath, SmoothPathRequest
 from geometry_msgs.msg import Twist, PoseStamped, Pose, Point
 
-NAME = "APELLIDO_PATERNO_APELLIDO_MATERNO"
+NAME = "SANCHEZ TORRES SERGIO DANIEL"
 
 pub_goal_reached = None
 pub_cmd_vel = None
@@ -42,6 +42,18 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
     # and return it (check online documentation for the Twist message).
     # Remember to keep error angle in the interval (-pi,pi]
     #
+    v_max = 0.7
+    w_max = 0.9
+    alpha = 1
+    beta = 1
+    error_a = math.atan2(goal_y-robot_y, goal_x-robot_x) - robot_a
+    error_a = ( (error_a + math.pi) % (2*math.pi) ) - math.pi
+
+    v = (v_max) * math.exp( -(error_a*error_a / alpha) )
+    w = (w_max) * ( ( 2/(1 + math.exp(-(error_a/beta))) ) - 1 )
+
+    cmd_vel.linear.x = v
+    cmd_vel.angular.z = w
     
     return cmd_vel
 
