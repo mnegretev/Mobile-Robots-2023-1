b60b52413bcc5e360702c9271589f916dda672ba
adrianaSung <adriana.herrera@ingenieria.unam.edu>
2022-10-12 17:12:52 -0500

    Modificando practica 5

--- catkin_ws/src/students/scripts/practice05.py
+++ catkin_ws/src/students/scripts/practice05.py
@@ -42,7 +42,20 @@ def calculate_control(robot_x, robot_y, robot_a, goal_x, goal_y):
     # and return it (check online documentation for the Twist message).
     # Remember to keep error angle in the interval (-pi,pi]
     #
-    
+    v_max= 0.6
+    w_max= 0.8
+    alpha=1
+    betha=1
+
+    error_a= math.atan2(goal_y-robot_y, goal_x-robot_x)-robot_a
+    error_a=((error_a + math.pi) % (2*math.pi))- math.pi
+
+    v = (v_max)*math.exp(-(error_a*error_a/alpha))
+    w = (w_max)*((2/(1 + math.exp(-(error_a/betha)))) - 1)
+
+    cmd_vel.linear.x=v
+    cmd_vel.angular.z=w
+
     return cmd_vel
 
 def follow_path(path):
