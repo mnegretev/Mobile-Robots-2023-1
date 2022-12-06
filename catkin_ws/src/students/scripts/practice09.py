18081bc9656ad32d5a1d96887b8de55b8b8df3a6
JuanGR-FI <juan60430@hotmail.com>
2022-12-05 22:15:36 -0600

    Avance Practica 9

--- catkin_ws/src/students/scripts/practice09.py
+++ catkin_ws/src/students/scripts/practice09.py
@@ -63,7 +63,13 @@ def forward_kinematics(q, Ti, Wi):
     #     Check online documentation of these functions:
     #     http://docs.ros.org/en/jade/api/tf/html/python/transformations.html
     #
-    x,y,z,R,P,Y = 0,0,0,0,0,0
+    H = tft.identity_matrix()
+    for i in range(len(q)):
+     H = tft.concatenate_matrices(H, Ti[i], tft.rotation_matrix(q[i], Wi[i]))
+    H = tft.concatenate_matrices(H, Ti[7])
+    x,y,z = H[0,3], H[1,3], H[2,3]
+    R,P,Y = list(tft.euler_from_matrix(H))
+    #x,y,z,R,P,Y = 0,0,0,0,0,0
     return numpy.asarray([x,y,z,R,P,Y])
 
 def jacobian(q, Ti, Wi):
