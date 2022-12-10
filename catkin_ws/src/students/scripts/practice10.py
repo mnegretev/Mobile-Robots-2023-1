a054bd9ef8dead924fa1680f613a66583a6e063d
danieltorres4 <dst990502@outlook.com>
2022-12-10 16:42:46 -0600

    Adding backpropagate() function

--- catkin_ws/src/students/scripts/practice10.py
+++ catkin_ws/src/students/scripts/practice10.py
@@ -14,7 +14,7 @@ import numpy
 import rospy
 import rospkg
 
-NAME = "FULL_NAME"
+NAME = "SANCHEZ TORRES SERGIO DANIEL"
 
 class NeuralNetwork(object):
     def __init__(self, layers, weights=None, biases=None):
@@ -74,6 +74,14 @@ class NeuralNetwork(object):
         #     nabla_b[-l] = delta
         #     nabla_w[-l] = delta*ylpT  where ylpT is the transpose of outputs vector of layer l-1
         #
+        delta = (y[-1] - yt) * (y[-1] - pow(y[-1],2))
+        nabla_w[-1] = delta * numpy.transpose(y[-2])
+        nabla_b[-1] = delta
+
+        for i in range(2, len(self.layer_sizes)):
+            delta = numpy.dot( numpy.transpose(self.weights[-i+1]), delta ) * (y[-i] - pow(y[-i], 2))
+            nabla_w[-i] = delta * numpy.transpose(y[-i - 1]) 
+            nabla_b[-i] = delta
 
         return nabla_w, nabla_b
 
