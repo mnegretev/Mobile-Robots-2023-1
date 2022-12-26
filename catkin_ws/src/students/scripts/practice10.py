dddb4414b8d62b9e9bc2f459f6dd64751f06ae8f
israel135 <nisaacisrael@uahoo.com>
2022-12-26 00:53:19 -0600

    Practica 9 y 10

--- catkin_ws/src/students/scripts/practice10.py
+++ catkin_ws/src/students/scripts/practice10.py
@@ -14,7 +14,7 @@ import numpy
 import rospy
 import rospkg
 
-NAME = "FULL_NAME"
+NAME = "Najera Santamaria Isaac Israel"
 
 class NeuralNetwork(object):
     def __init__(self, layers, weights=None, biases=None):
@@ -51,6 +51,11 @@ class NeuralNetwork(object):
         # Include input x as the first output.
         #
         y = []
+        y.append(x)
+        for i in range(len(self.biases)):
+        	z = numpy.dot(self.weights[i], x) + self.biases[i]
+        	x = 1.0/(1.0 + numpy.exp(-z))
+        	y.append(x)
         return y
 
     def backpropagate(self, x, yt):
@@ -74,7 +79,13 @@ class NeuralNetwork(object):
         #     nabla_b[-l] = delta
         #     nabla_w[-l] = delta*ylpT  where ylpT is the transpose of outputs vector of layer l-1
         #
-
+        delta = (y[-1] - yt)*(y[-1])*(1 - y[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = delta * numpy.transpose(y[-2])
+        for l in range(2, self.num_layers):
+               delta = numpy.dot(numpy.transpose(self.weights[-l+1]),delta) * y[-l] * (1-y[-l])
+               nabla_b[-l] = delta
+               nabla_w[-l] = numpy.dot(delta, numpy.transpose(y[-l-1]))
         return nabla_w, nabla_b
 
     def update_with_batch(self, batch, eta):
