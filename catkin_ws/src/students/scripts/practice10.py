299751688ee9474f5e58cc45988b19ba6afba1da
DanielChavolla <danchavollaing@gmail.com>
2023-01-03 17:59:15 -0600

    correccionpractica10

--- catkin_ws/src/students/scripts/practice10.py
+++ catkin_ws/src/students/scripts/practice10.py
@@ -51,6 +51,11 @@ class NeuralNetwork(object):
         # Include input x as the first output.
         #
         y = []
+        y.append(x)
+        for i in range(len(self.biases)):
+            z = numpy.dot(self.weights[i], x) + self.biases[i]
+            x = 1.0 / (1.0 + numpy.exp(-z))  #output of the current layer is the input of the next one
+            y.append(x)
         return y
 
     def backpropagate(self, x, yt):
@@ -74,6 +79,16 @@ class NeuralNetwork(object):
         #     nabla_b[-l] = delta
         #     nabla_w[-l] = delta*ylpT  where ylpT is the transpose of outputs vector of layer l-1
         #
+        y = self.feedforward_verbose(x)
+        nabla_b = [numpy.zeros(b.shape) for b in self.biases]
+        nabla_w = [numpy.zeros(w.shape) for w in self.weights]
+        delta = (y[-1] - yt)*y[-1]*(1 - y[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = delta*y[-2].transpose()
+        for i in range(2, self.num_layers):
+            delta = numpy.dot(self.weights[-i+1].transpose(), delta)*y[-i]*(1.0 - y[-i])
+            nabla_b[-i] = delta
+            nabla_w[-i] = numpy.dot(delta,y[-i-1].transpose())
 
         return nabla_w, nabla_b
 
