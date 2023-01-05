f1c14ca5b03de0f0059c362de6a9e1e3291aab13
Brayan Alexis Medina Vazquez <47645744+AlexisMedina99@users.noreply.github.com>
2023-01-05 17:57:29 -0600

    Update et_facta_est_lux.py

--- catkin_ws/src/students/scripts/et_facta_est_lux.py
+++ catkin_ws/src/students/scripts/et_facta_est_lux.py
@@ -29,7 +29,7 @@ from sound_play.msg import SoundRequest
 from custom_msgs.srv import *
 from custom_msgs.msg import *
 
-NAME = "FULL NAME"
+NAME = "MEDINA VAZQUEZ BRAYAN ALEXIS"
 
 #
 # Global variable 'speech_recognized' contains the last recognized sentence
@@ -240,6 +240,77 @@ def main():
     #
     
     while not rospy.is_shutdown():
+        if current_state == "SM_INIT": #State 0
+            print("Starting state machine :D ...")
+            if(new_task == True):
+                new_task = False
+                current_state = "SM_SAY_HELLO"
+
+        elif current_state == "SM_SAY_HELLO": #STATE 1
+            print("Saying hello")
+            say("Hello world")
+            #current_state = "SM_MOVE_ARM"
+            current_state = "SM_OBJECT_DETECTION"
+
+        elif current_state == "SM_OBJECT_DETECTION":#STATE 2
+            print("Recognizing Object")
+            say("Robot is recognizing object")
+            obj,loc = parse_command(recognized_speech) #Se reconce el voz
+            print(obj)
+            print(loc)
+            current_state = "SM_HEAD_DOWN"
+
+        elif current_state == "SM_HEAD_DOWN":#STATE 3
+            print("Moving head")
+            say("Im sad") #CAMBIAR
+            move_head(0,-0.9)
+            current_state = "SM_FIND_OBJECT"
+
+        elif current_state == "SM_FIND_OBJECT":
+            print("Finding object")
+            say("Finding object")
+            x_obj, y_obj, z_obj = find_object(obj)
+            print(x_obj, y_obj, z_obj)
+            current_state == "SM_POINT_TRANSFORM"
+
+        elif current_state == "SM_POINT_TRANSFORM":
+            if obj == "pringles":
+                xt, yt, zt = transform_point_to_left_arm(x_obj, y_obj, z_obj)
+
+            else:
+                xt, yt, zt = transform_point_to_right_arm(x_obj, y_obj, z_obj)
+            print(xt, yt, zt)
+            current_state = "SM_INVERSE_KINEMATICS"
+
+        elif current_state == "SM_INVERSE_KINEMATICS":
+            if obj == "pringles":
+                q = ik_left_arm(xt,yt,zt)
+            else:
+                q = ik_right_arm(xt,yt,zt)
+            print(q)
+            current_state == "SM_MOVE_ARM_TO_START"
+
+        elif current_state == "SM_MOVE_ARM_TO_START":
+             print("Moving the robot's arm")
+             move_left_arm(-0.5,0,0,2.1,0,0,0)
+             current_state = "SM_MOVE_ARM_TO_TAKE_OBJ"
+
+        elif current_state == "SM_MOVE_ARM_TO_TAKE_OBJ":
+            print("moving_arm_obj")
+            say("Robot is moving its arm")
+            if obj == "pringles":
+                move_left_arm(q[0],q[1],q[2],q[3],q[4],q[5],q[6])
+            else:
+                move_right_arm(q[0],q[1],q[2],q[3],q[4],q[5],q[6])
+            current_state = "SM_CLOSE_HAND"
+        
+        elif current_state == "SM_CLOSE_HAND":
+            print("closing hand")
+            if obj == "pringles":
+                move_left_gripper(q)
+            else:
+                move_right_gripper(q)
+            current_state == "SM_FINISH"
         loop.sleep()
 
 if __name__ == '__main__':
