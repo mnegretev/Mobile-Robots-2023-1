1d7a208e07823a502549e89490cbf274ebeddb4e
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2023-01-05 13:11:18 -0600

    Correct files for speech recognition node

---
+++ catkin_ws/Push3.sh
@@ -0,0 +1,208 @@
+git checkout pedraza_martinez
+git pull origin pedraza_martinez
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout tapia_solis
+git pull origin tapia_solis
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout lopez_cruz
+git pull origin lopez_cruz
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout miranda_hernandez
+git pull origin miranda_hernandez
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout najera_araoz
+git pull origin najera_araoz
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout reyes_gonzalez
+git pull origin reyes_gonzalez
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout soto_monterroza
+git pull origin soto_monterroza
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout torres_marin
+git pull origin torres_marin
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout sobrevilla_zarazua
+git pull origin sobrevilla_zarazua
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout romero_morales
+git pull origin romero_morales
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout suarez_martinez
+git pull origin suarez_martinez
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout sanchez_torres
+git pull origin sanchez_torres
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout cervantes_guati
+git pull origin cervantes_guati
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout diaz_garcia
+git pull origin diaz_garcia
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout chavolla_jimenez
+git pull origin chavolla_jimenez
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout ginez_alvarez
+git pull origin ginez_alvarez
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout sanchez_gaona
+git pull origin sanchez_gaona
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout coria_perez
+git pull origin coria_perez
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout troncoso_moreno
+git pull origin troncoso_moreno
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout vazquez_zaragoza
+git pull origin vazquez_zaragoza
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout medina_vazquez
+git pull origin medina_vazquez
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout reyes_alonso
+git pull origin reyes_alonso
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout herrera_godina
+git pull origin herrera_godina
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout gutierrez_orozpe
+git pull origin gutierrez_orozpe
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout martinez_juarez 
+git pull origin martinez_juarez
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
+git checkout najera_santamaria
+git pull origin najera_santamaria
+cp ~/temp/CMakeLists.txt src/custom_msgs/CMakeLists.txt 
+cp ~/temp/RecognizedSpeech.msg src/custom_msgs/msg/
+git add .
+git commit -m "Correct files for final project"
+#git merge master
+
