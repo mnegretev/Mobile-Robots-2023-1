a23496230e45320d06108e159b6bc0048c6197bf
DanielChavolla <danchavollaing@gmail.com>
2023-01-11 10:49:56 -0600

    proyectoFinal

--- catkin_ws/src/students/scripts/et_facta_est_lux.py
+++ catkin_ws/src/students/scripts/et_facta_est_lux.py
@@ -29,16 +29,18 @@ from sound_play.msg import SoundRequest
 from custom_msgs.srv import *
 from custom_msgs.msg import *
 
-NAME = "CHAVOLLA JIMENEZ RAUL DANIEL"
+NAME = "Chavolla Jimenez Raul Daniel"
 
 #
 # Global variable 'speech_recognized' contains the last recognized sentence
 #
 def callback_recognized_speech(msg):
     global recognized_speech, new_task, executing_task
+        
     if executing_task:
         return
     new_task = True
+    print(msg.data)
     recognized_speech = msg.data
 
 #
@@ -51,6 +53,7 @@ def callback_goal_reached(msg):
 def parse_command(cmd):
     obj = "pringles" if "PRINGLES" in cmd else "drink"
     loc = [8.0,8.5] if "TABLE" in cmd else [3.22, 9.72]
+    
     return obj, loc
 
 #
@@ -68,7 +71,7 @@ def move_left_arm(q1,q2,q3,q4,q5,q6,q7):
     msg.data.append(q6)
     msg.data.append(q7)
     pubLaGoalPose.publish(msg)
-    time.sleep(5.0)
+    time.sleep(2.0)
 
 #
 # This function sends the goal angular position to the left gripper and sleeps 1 second
@@ -94,7 +97,7 @@ def move_right_arm(q1,q2,q3,q4,q5,q6,q7):
     msg.data.append(q6)
     msg.data.append(q7)
     pubRaGoalPose.publish(msg)
-    time.sleep(5.0)
+    time.sleep(2.0)
 
 #
 # This function sends the goal angular position to the right gripper and sleeps 1 second
@@ -115,7 +118,7 @@ def move_head(pan, tilt):
     msg.data.append(pan)
     msg.data.append(tilt)
     pubHdGoalPose.publish(msg)
-    time.sleep(1.0)
+    time.sleep(5.0)
 
 #
 # This function sends a linear and angular speed to the mobile base to perform
@@ -123,7 +126,6 @@ def move_head(pan, tilt):
 # during a time given by 't'
 #
 def move_base(linear, angular, t):
-   
     global pubCmdVel
     cmd = Twist()
     cmd.linear.x = linear
@@ -138,7 +140,6 @@ def move_base(linear, angular, t):
 #
 def go_to_goal_pose(goal_x, goal_y):
     global pubGoalPose
-  
     goal_pose = PoseStamped()
     goal_pose.pose.orientation.w = 1.0
     goal_pose.pose.position.x = goal_x
@@ -159,18 +160,19 @@ def say(text):
     pubSay.publish(msg)
 
 def transform_point_to_left_arm(x,y,z):
+    global obj_p
     listener = tf.TransformListener()
     listener.waitForTransform("shoulders_left_link", "kinect_link", rospy.Time(), rospy.Duration(4.0))
     obj_p = PointStamped()
     obj_p.header.frame_id = "kinect_link"
     obj_p.header.stamp = rospy.Time(0)
-    obj_p.point.x, obj_p.point.y, obj_p.point.z = x,y,z 
-
+    obj_p.point.x, obj_p.point.y, obj_p.point.z = x,y,z
     target_frame = "shoulders_left_link"
     obj_p = listener.transformPoint(target_frame, obj_p)
     return obj_p.point.x, obj_p.point.y, obj_p.point.z
 
 def transform_point_to_right_arm(x,y,z):
+    global obj_p
     listener = tf.TransformListener()
     listener.waitForTransform("shoulders_left_link", "kinect_link", rospy.Time(), rospy.Duration(4.0))
     obj_p = PointStamped()
@@ -182,16 +184,18 @@ def transform_point_to_right_arm(x,y,z):
     return obj_p.point.x, obj_p.point.y, obj_p.point.z
 
 def find_object(obj_name):
-    time.sleep(2.0)
+    global resp_find_object
     clt_find_object = rospy.ServiceProxy("/vision/find_object", FindObject)
     req_find_object = FindObjectRequest()
     req_find_object.cloud = rospy.wait_for_message("/kinect/points", PointCloud2)
     req_find_object.name  = obj_name
     resp_find_object = clt_find_object(req_find_object)
-    print("Object found at: " + str([resp_find_object.x, resp_find_object.y, resp_find_object.z]))
-    return resp_find_object.x, resp_find_object.y, resp_find_object.z
+    
+
+    return (resp_find_object.x, resp_find_object.y, resp_find_object.z)
 
 def ik_left_arm(x,y,z):
+    global resp_ik
     clt_la_inverse_kin = rospy.ServiceProxy("/manipulation/la_inverse_kinematics", InverseKinematics)
     req_ik = InverseKinematicsRequest()
     req_ik.x, req_ik.y, req_ik.z = x,y,z
@@ -200,6 +204,7 @@ def ik_left_arm(x,y,z):
     return resp_ik.q1, resp_ik.q2, resp_ik.q3, resp_ik.q4, resp_ik.q5, resp_ik.q6, resp_ik.q7
 
 def ik_right_arm(x,y,z):
+    global resp_ik
     clt_ra_inverse_kin = rospy.ServiceProxy("/manipulation/ra_inverse_kinematics", InverseKinematics)
     req_ik = InverseKinematicsRequest()
     req_ik.x, req_ik.y, req_ik.z = x,y,z
@@ -239,127 +244,122 @@ def main():
     recognized_speech = ""
     goal_reached = False
 
+    
+
     current_state = "SM_INIT"
     requested_object   = ""
     requested_location = [0,0]
-
     #
     # FINAL PROJECT
-    # SE AGREGA LA MAQUINA DE ESTADOS
+    # 
     #
-  
+    orden=""
+    xf=0
+    yf=0
+    zf=0
+    xt=0
+    yt=0
+    zt=0
+    q1=0
+    q2=0
+    q3=0
+    q4=0
+    q5=0
+    q6=0
+    q7=0
+    
     while not rospy.is_shutdown():
-        if current_state == "SM_INIT": #State 0
-            print("Starting state machine :D ...")
-            if(new_task == True):
-                new_task = False
-                current_state = "SM_SAY_HELLO"
-
-        elif current_state == "SM_SAY_HELLO": #STATE 1
-            print("Saying hello")
-	    
-            say("Hello world")
-            #current_state = "SM_MOVE_ARM"
-            current_state = "SM_OBJECT_DETECTION"
-
-        elif current_state == "SM_OBJECT_DETECTION":#STATE 2
-            print("Recognizing Object")
-            say("Robot is recognizing object")
-            obj,loc = parse_command(recognized_speech) #Se reconce el voz
-            print(obj)
-            print(loc)
-            current_state = "SM_HEAD_DOWN"
-
-        elif current_state == "SM_HEAD_DOWN":#STATE 3
-            print("Moving head")
-            say("Im sad") #CAMBIAR
-            move_head(0,-0.9)
-            current_state = "SM_FIND_OBJECT"
-
-        elif current_state == "SM_FIND_OBJECT":
-            print("Finding object")
-            say("Finding object")
-            x_obj, y_obj, z_obj = find_object(obj)
-            print(x_obj, y_obj, z_obj)
-            current_state = "SM_POINT_TRANSFORM"
-
-        elif current_state == "SM_POINT_TRANSFORM":
-	    print("transformacion")
-            if obj == "pringles":
-                xt, yt, zt = transform_point_to_left_arm(x_obj, y_obj, z_obj)
-		
-            else:
-                xt, yt, zt = transform_point_to_right_arm(x_obj, y_obj, z_obj)
-            print(xt, yt, zt)
-            current_state = "SM_INVERSE_KINEMATICS"
-
-        elif current_state == "SM_INVERSE_KINEMATICS":
-	    print("SIK")
-            if obj == "pringles":
-                q = ik_left_arm(xt,yt,zt)
-		print("Llegue")
-            else:
-                q = ik_right_arm(xt,yt,zt)
-            print(q)
-
-            current_state = "SM_MOVE_ARM_TO_START"
-
-        elif current_state == "SM_MOVE_ARM_TO_START":
-             print("Moving the robot's arm")
-             print("posicion inicial")
-	     move_left_arm(-0.5,0,0,2.3,0,0,0)
-             current_state = "SM_MOVE_ARM_TO_TAKE_OBJ"
-
-        elif current_state == "SM_MOVE_ARM_TO_TAKE_OBJ":
-            print("moving_arm_obj")
-            say("Robot is moving its arm")
-            if obj == "pringles":
-		move_left_gripper(0.5)
-                move_left_arm(q[0],q[1],q[2],q[3],q[4],q[5],q[6])
-            else:
-		move_right_gripper(0.5)
-                move_right_arm(q[0],q[1],q[2],q[3],q[4],q[5],q[6])
-            current_state = "SM_CLOSE_HAND"
-        
-        elif current_state == "SM_CLOSE_HAND":
-            print("closing hand")
-            if obj == "pringles":
-                move_left_arm(-0,0,0,1.7,0,0,0)
-                move_left_gripper(-0.5)
-		move_left_arm(0,0,0,3,0,0,-1)
- 	    else:
-		move_right_arm(-0,0,0,1.7,0,0,0)
-                move_right_gripper(-0.5)
-		move_right_arm(0,0,0,3,0,0,-0.8)
-		
-	    say("I got it")
-            #else:
-                #move_right_gripper(-3)
-	    
+        if current_state=="SM_INIT":
+            print("Inicio de la maquina de estados")
+            #requested_object=callback_recognized_speech
+            current_state="SM_SAY_HELLO"
             
-	   
-	    current_state = "SM_MOVE_TO_GOAL_POSE"
-	elif current_state == "SM_MOVE_TO_GOAL_POSE" :
-	   go_to_goal_pose(loc[0],loc[1])
-	   if goal_reached:
-	   	if obj == "pringles":
-	   		move_left_gripper(0.5)
-	   current_state = "SM_RETURN"
-        elif current_state == "SM_RETURN" :
-            go_to_goal_pose(3.32,5.86)
-	    print("estoy girando")
-	    move_base(0,1,15)
-	    print("ya gire")
-	    move_left_gripper(0)
+        elif current_state=="SM_SAY_HELLO":
+            if new_task:
+                say("Hello")
+                requested_object,requested_location=parse_command(recognized_speech)
+                current_state="SM_MOVE_HEAD"
+                new_task=False
+                executing_task=True
 
+        elif current_state=="SM_MOVE_HEAD":
+            move_head(0,-0.8)
+            #move_right_arm(-0.3,0,0,1.8,0,0,0)
+            #move_left_arm(-0.3,0,0,2.0,0,0,0)
+            #move_base(-0.5,0.0,1.0)
+            current_state="SM_FIND_OBJECT"
+
+        elif current_state=="SM_FIND_OBJECT":
+            print ("Coordenadas de las pringles")
+            find_object("pringles")
+            print("Coordenadas del pisto")
+            find_object("drink")
+            say("Found")
+            time.sleep(1.0)
+            current_state="SM_TRANSFORM_OBJ"
+
+        elif current_state=="SM_TRANSFORM_OBJ":
+            if requested_object=="pringles":
+                print ("Entrando al estado para aplicar la trasformada de los objetos")
+                xf,yf,zf=find_object("pringles")
+                print("Transformamos al brazo")
+                xt,yt,zt=transform_point_to_left_arm(xf,yf,zf)
+                print("Cinematica inversa")
+                q1m,q2m,q3m,q4m,q5m,q6m,q7m=ik_left_arm(xt,yt,zt)
+                current_state="SM_MOVE_ARM_LEFT"
+            else:
+                print("Transformamos al otro brazo")
+                xfd,yfd,zfd=find_object("drink")
+                xt,yt,zt=transform_point_to_right_arm(xfd,yfd,zfd)
+                print("Cinematica inversa para el otro brazo")
+                q1m,q2m,q3m,q4m,q5m,q6m,q7m=ik_right_arm(xt,yt,zt)
+                print ("Cinematica inversa para el otro brazo")
+                current_state="SM_MOVE_ARM_RIGHT"
+
+        elif current_state =="SM_MOVE_ARM_LEFT":
+            q1,q2,q3,q4,q5,q6,q7=ik_left_arm(0.20,0.06,-0.25)
+            move_left_arm(q1,q2,q3,q4,q5,q6,q7)
+            move_left_gripper(0.6)
+            move_left_arm(q1m,q2m,q3m,q4m,q5m,q6m,q7m)
+            move_left_gripper(-0.3)
+            q1,q2,q3,q4,q5,q6,q7=ik_left_arm(0.20,0.06,-0.25)
+            move_left_arm(q1,q2,q3,q4,q5,q6,q7)
+            current_state="SM_MOVE_POSE"
+
+        elif current_state =="SM_MOVE_ARM_RIGHT":
+            q1,q2,q3,q4,q5,q6,q7=ik_right_arm(0.20,0.06,-0.25)
+            move_right_arm(q1,q2,q3,q4,q5,q6,q7)
+            move_right_gripper(0.6)
+            move_right_arm(q1m,q2m,q3m,q4m,q5m,q6m,q7m)
+            move_right_gripper(-0.3)
+            q1,q2,q3,q4,q5,q6,q7=ik_right_arm(0.20,0.06,-0.25)
+            move_right_arm(q1,q2,q3,q4,q5,q6,q7)
+            current_state="SM_MOVE_POSE_DR"
+                           
+        elif current_state=="SM_MOVE_POSE":
+            move_head(0.0,0.0)
+            move_base(-1,0,2)
+            current_state="SM_MOVE_KITCHEN"
+
+        elif current_state=="SM_MOVE_POSE_DR":
+            move_head(0.0,0.0)
+            move_base(-1,0,2)
+            current_state=="SM_MOVE_TABLE"
             
-            current_state = "SM_FINISH"
-	
-         
- 
+        elif current_state=="SM_MOVE_KITCHEN":
+            go_to_goal_pose(6,6.2)
+            current_state="SM_MOVE_HOME"
+
+        elif current_state=="SM_MOVE_TABLE":
+            go_to_goal_pose(3.4,9)
+            current_state="SM_MOVE_HOME"
+
+            executing_task=False
+                        
+        loop.sleep()
 if __name__ == '__main__':
     try:
         main()
     except rospy.ROSInterruptException:
         pass
-  
+    
